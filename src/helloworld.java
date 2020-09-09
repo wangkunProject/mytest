61f2ad474da048a603ede77f3792a423d1ff05fd
wangkunProject <2295013046@qq.com>
2020-09-09 18:24:05 +0800

    Initial commit

---
+++ src/helloworld.java
@@ -0,0 +1,20 @@
+public class helloworld {
+    String name;
+    int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
